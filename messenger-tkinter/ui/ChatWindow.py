89fb882a891ac998d422e7e8274a7cfb4b5c5b3e
gjergjk71 <gjergjk71@gmail.com>
2018-09-10 15:43:20 +0200

    Switched from pack to place

--- messenger-tkinter/ui/ChatWindow.py
+++ messenger-tkinter/ui/ChatWindow.py
@@ -7,25 +7,25 @@ class ChatWindow():
 	def setupChatUI(self,master):
 		l1 = tk.Label(master,text="Messenger",font=("",50))
 
-        txt_frm = tk.Frame(self.root, width=600, height=600)
-        txt_frm.pack(fill="both", expand=True)
-        txt_frm.grid_propagate(False)
+		txt_frm = tk.Frame(self.master, width=600, height=600)
 
-        txt_frm.grid_rowconfigure(0, weight=1)
-        txt_frm.grid_columnconfigure(0, weight=1)
-
-        self.txt = tk.Text(txt_frm, borderwidth=3, relief="sunken")
-        self.txt.config(font=("consolas", 12), undo=True, wrap='word')
-        self.txt.grid(row=0, column=0, sticky="nsew", padx=2, pady=2)
-
-        scrollb = tk.Scrollbar(txt_frm, command=self.txt.yview)
-        scrollb.place(row=0, column=1, sticky='nsew')
-        self.txt['yscrollcommand'] = scrollb.set
+		txt = tk.Text(txt_frm, borderwidth=3, relief="sunken")
+		for i in range(50):
+			txt.insert(tk.END,"DSADASD\n")
+		txt.config(state="disabled")
+		txt.config(font=("consolas", 12), undo=True, wrap='word')
+		scrollb = tk.Scrollbar(txt_frm, command=txt.yview)
+		txt['yscrollcommand'] = scrollb.set
 		self.ChatWidgets = {"l1":l1,
-							"t1":t1}
+							"txt_frm":txt_frm,
+							"txt":txt,
+							"scrollb":scrollb}
 	def showChat(self):
-		self.ChatWidgets["t1"].place(x=50,y=50)
-		self.ChatWidgets["t1"].place(x=10,y=10)
+		self.ChatWidgets["txt_frm"].place(x=50,y=50)
+		self.ChatWidgets["l1"].place(x=60,y=60)
+		self.ChatWidgets["scrollb"].place(x=100,y=100)
+		self.ChatWidgets["txt"].place(x=100,y=100)
+
 	def hideChat(self):
 		for widget_name,widget_instance in self.ChatWidgets.items():
 			widget_instance.place_forget()
